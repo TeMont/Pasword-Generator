236303e4c0fde0e5813c9206c30a110f649f2536
TeMont <150724079+TeMont@users.noreply.github.com>
2024-01-30 18:47:58 +0400

    Update lib.hpp

--- lib.hpp
+++ lib.hpp
@@ -1,7 +1,10 @@
 #include <vector>
 
-std::vector<std::vector<char>> CharactersList;
+std::vector<std::vector<char>> UsingCharactersList;
+
 std::vector<char> UpperCaseLetters {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 std::vector<char> LowerCaseLetters {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 std::vector<char> Digits {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-std::vector<char> SpecialCharacters	{'!', '@', '#', '$', '%', '^', '&', '*'};
+std::vector<char> SpecialCharacters {'!', '@', '#', '$', '%', '^', '&', '*'};
+
+std::vector<std::vector<char>> CharactersList {UpperCaseLetters, LowerCaseLetters, Digits, SpecialCharacters};
